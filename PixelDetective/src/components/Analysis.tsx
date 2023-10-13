983b4a6c18ec805e31fb25c3f889ea39071a6231
Antoine Smet <antoinesmetdev@gmail.com>
2023-10-13 17:17:19 +0200

    dense captions  end

--- PixelDetective/src/components/Analysis.tsx
+++ PixelDetective/src/components/Analysis.tsx
@@ -1,7 +1,66 @@
+import React, { useState } from "react";
+import axios from "axios";
 
 function Analysis() {
-    return <div>Analysis</div>;
-  }
-  
-  export default Analysis;
-  
+  const [image, setImage] = useState<File | null>(null);
+  const [captions, setCaptions] = useState<string[]>([]);
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      setImage(file);
+    }
+  };
+
+  const sendImageToAzure = async () => {
+    try {
+      if (image) {
+        const reader = new FileReader();
+        reader.onload = async () => {
+          try {
+            const binaryData = reader.result as ArrayBuffer;
+            const response = await axios.post(
+              "endpoint/computervision/imageanalysis:analyze?api-version=2023-02-01-preview&features=denseCaptions",
+              binaryData,
+              {
+                headers: {
+                  "Ocp-Apim-Subscription-Key":
+                    "key",
+                  "Content-Type": "application/octet-stream",
+                },
+              }
+            );
+
+            response.data.denseCaptionsResult.values.forEach(
+              (value: { text: string }) => {
+                setCaptions((prevCaptions) => [...prevCaptions, value.text]);
+              }
+            );
+          } catch (error) {
+            console.error("Erreur lors de la requête Azure : ", error);
+          }
+        };
+
+        reader.readAsArrayBuffer(image);
+      } else {
+        console.error("Aucune image sélectionnée.");
+      }
+    } catch (error) {
+      console.error("Erreur lors de l'envoi de l'image à Azure : ", error);
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleImageChange} />
+      <button onClick={sendImageToAzure}>🔎</button>
+      <ul>
+        {captions.map((caption, index) => (
+          <li key={index}>{caption}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Analysis;
